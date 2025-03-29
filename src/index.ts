dae512bc29b5158e498083b734e1b5492b632af2
Lizard <li0ard@proton.me>
2025-03-29 14:09:50 +0600

    feat: v0.2.8 | Add export for `recoveryNested`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { recovery32, recovery64, lfsr_recovery32, lfsr_recovery64, lfsr_rollback_bit, lfsr_rollback_byte, lfsr_rollback_word } from "./crapto1"
+export { recovery32, recovery64, recoveryNested, lfsr_recovery32, lfsr_recovery64, lfsr_rollback_bit, lfsr_rollback_byte, lfsr_rollback_word } from "./crapto1"
 export { Crypto1State } from "./state"
 export { prng_successor, crypto1_bit, crypto1_byte, crypto1_word, encrypt } from "./crypto1"
 export { oddParity8, bebit, bit, filter, parity, binsearch, quicksort, update_contribution, extend_table, extend_table_simple, evenParity32, evenParity8, swapendian } from "./utils"
